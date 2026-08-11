3c7f538b43ebe2c7bc9d7cecef3b2ea85bd3e569
Jostrod <jostroda@stud.ntnu.no>
2026-08-11 13:39:09 +0200

    feat: set up initial endpoints

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,19 @@ import express from "express";
 const app = express()
 
 app.get("/api/notes", (req, res)=> {
-    res.send("You got 5 notes")
+    res.send("You got 50 notes")
+})
+
+app.post("/api/notes", (req, res) =>{
+    res.status(201).json({message:"Post created successfully"})
+})
+
+app.put("/api/notes/:id", (req, res) =>{
+    res.status(200).json({message:"Post updated successfully"})
+})
+
+app.delete("/api/notes/:id", (req, res) =>{
+    res.status(200).json({message:"Post deleted successfully"})
 })
 
 app.listen(5001, ()=> {
